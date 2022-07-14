d4745ac9befeda819f3c91db9a6537fa8412fbad
Sergey Goncharov <goncharovserg@yandex-team.ru>
2022-07-14 18:51:18 +0300

    bugfix: next fix some bugs after review

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 //инициализацию JS-кода, добавление слушателей и другие важные участки
 
-import * as style from '../pages/index.css'; // добавьте импорт главного файла стилей
+import * as style from '../pages/index.css'
 import * as modal from './modal.js'
 import * as validate from './validate.js'
 import * as requests from './requests.js'
